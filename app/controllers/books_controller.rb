ee22a4843f787cae6c2778c9e44be74ca186caac
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-09-17 01:49:18 +0000

    詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def new
-    @books = Book.new
+    @book = Book.new
   end
 
   def create
@@ -11,9 +11,11 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
   end
 
   # 投稿データのストロングパラメータ
