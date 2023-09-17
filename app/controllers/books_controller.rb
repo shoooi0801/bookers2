45b8f8ba407ff8301d76ce31c62ede9705290122
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-09-17 08:02:33 +0000

    バリデージョン

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,12 +19,26 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
+    else
+       render "edit"
+    end
+  end
+
   def show
     @book = Book.find(params[:id])
   end
 
   def index
     @books = Book.all
+  end
+
+  def edit
+    @book = Book.find(params[:id])
   end
 
   # 投稿データのストロングパラメータ
