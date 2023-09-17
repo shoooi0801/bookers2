864859241510e08160c3df2734dd4000a471bcc2
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-09-17 02:16:20 +0000

    削除まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,12 @@ class BooksController < ApplicationController
     redirect_to books_path
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   def show
     @book = Book.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :books, only: [:new, :index, :show, :create]
+  resources :books, only: [:new, :index, :show, :create, :destroy]
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
