0f2f10a682aff011c5e52cd92a3542c06d64141a
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-09-17 07:15:57 +0000

    バリデージョンまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
+    if @book.save
     redirect_to books_path
+    else
+    render :new
+    end
   end
 
   def destroy
@@ -29,5 +32,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
