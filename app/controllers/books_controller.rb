a555b7b0dc3795b40096cc09cdcdd4828825e29d
shoi <138567675+shoooi0801@users.noreply.github.com>
2023-09-19 10:08:58 +0000

    フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else
       @user = current_user
@@ -15,12 +16,14 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "You have destroyed book successfully."
     redirect_to books_path
   end
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
